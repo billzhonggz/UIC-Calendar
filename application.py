5b042aabb461612711c508c78960070f0dccaba0
Bill ZHONG <billzhonggz@outlook.com>
2019-03-28 10:27:34 +0800

    Add login form functions.

--- application.py
+++ application.py
@@ -2,7 +2,10 @@ import datetime
 
 from flask import Flask, render_template, request
 
+from wtforms import Form, StringField, PasswordField, validators
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -19,10 +22,24 @@ def index():
     # Render the template with arguments.
     return render_template('index.html', date=date, events=events)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        pass
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = form.username
+        password = form.password
+        if username.data == 'admin' and password.data == 'admin':
+            message = 'Login succeed, taking you to the admin page.'
+            return render_template('login.html', message=message)
+        else:
+            message = 'Login failed.'
+            return render_template('login.html', message=message)
     else:
-        message = 'Test message'
+        message = 'Test username: admin, password: admin'
         return render_template('login.html', message=message)
+
+
+class LoginForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('Password', [validators.DataRequired()])
